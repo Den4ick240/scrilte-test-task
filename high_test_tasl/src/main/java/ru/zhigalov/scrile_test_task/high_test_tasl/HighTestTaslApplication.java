1ebe72a6d382e1d900087ae5234f07bf39c86c09
den4ick240 <zhigalovden@mail.ru>
2022-06-15 15:12:35 +0700

    Created project

---
+++ high_test_tasl/src/main/java/ru/zhigalov/scrile_test_task/high_test_tasl/HighTestTaslApplication.java
@@ -0,0 +1,13 @@
+package ru.zhigalov.scrile_test_task.high_test_tasl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighTestTaslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HighTestTaslApplication.class, args);
+	}
+
+}
---
+++ high_test_tasl/src/test/java/ru/zhigalov/scrile_test_task/high_test_tasl/HighTestTaslApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zhigalov.scrile_test_task.high_test_tasl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighTestTaslApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
